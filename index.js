0f3320ddc714ea33369b9b220b884dd8416c19f2
Jesus Seijas <jseijas@gmail.com>
2017-03-04 23:00:19 +0100

    Added custom slack connector using the multi connector.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const BotManager = require('flow-bot-manager').default;
 const restify = require('restify');
+const FlowConnectorSlack = require('flow-connector').FlowConnectorSlack;
 
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
+
 var opts = {
   defaultLocale: 'en',
   localesPath: './bot/locales',
@@ -12,5 +14,11 @@ var opts = {
   actionPath: './bot/actions',
   dialogPath: './bot/dialogs'
 };
+
+var slack = new FlowConnectorSlack({
+  botToken: process.env.BOT_SLACK_TOKEN
+});
+
 server.botManager = new BotManager(opts);
+server.botManager.connector.addConnector('slack', slack);
 server.post('/api/messages', server.botManager.connector.listen());
